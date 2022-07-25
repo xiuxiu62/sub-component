707de7e1905be6609b1fadb70f222b0029b74041
xiuxiu62 <jacremer@live.com>
2022-07-25 01:39:16 -0700

    Initial

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,66 @@
-use num::One;
+use num::{
+    traits::{WrappingAdd, WrappingSub},
+    One,
+};
 use std::ops::{Add, AddAssign, Sub, SubAssign};
 
-#[derive(Copy, Clone, Debug)]
-struct Number<T>(T);
+#[derive(Debug, Default)]
+#[repr(transparent)]
+pub struct SubComponent<T>(T);
 
-impl<T: Copy + One + Add<Output = T> + Sub<Output = T>> Number<T> {
+impl<T> SubComponent<T>
+where
+    T: Copy
+        + One
+        + Min
+        + Max
+        + Ord
+        + Add<Output = T>
+        + Sub<Output = T>
+        + WrappingAdd<Output = T>
+        + WrappingSub<Output = T>,
+{
+    #[inline]
     pub fn new(value: T) -> Self {
         Self(value)
     }
 
+    #[inline]
     pub fn get(&self) -> T {
         self.0
     }
 
+    #[inline]
     pub fn set(&mut self, value: T) {
         self.0 = value;
     }
 
+    #[inline]
     pub fn increment(&mut self) {
-        *self += num::one();
+        self.wrapping_add(num::one());
     }
 
+    #[inline]
     pub fn decrement(&mut self) {
-        *self -= num::one();
+        self.wrapping_sub(num::one());
+    }
+
+    #[inline]
+    pub fn wrapping_add(&mut self, value: T) {
+        self.set(self.0.wrapping_add(&value));
+    }
+
+    #[inline]
+    pub fn wrapping_sub(&mut self, value: T) {
+        self.set(self.0.wrapping_sub(&value));
+    }
+    #[inline]
+    pub fn reset(&mut self) {
+        self.set(<T as Min>::min())
     }
 }
 
-impl<T: Add<Output = T>> Add<T> for Number<T> {
+impl<T: Add<Output = T>> Add<T> for SubComponent<T> {
     type Output = T;
 
     fn add(self, rhs: T) -> Self::Output {
@@ -34,13 +68,13 @@ impl<T: Add<Output = T>> Add<T> for Number<T> {
     }
 }
 
-impl<T: Copy + Add<Output = T>> AddAssign<T> for Number<T> {
+impl<T: Copy + Add<Output = T>> AddAssign<T> for SubComponent<T> {
     fn add_assign(&mut self, rhs: T) {
         *self = Self(self.0 + rhs);
     }
 }
 
-impl<T: Sub<Output = T>> Sub<T> for Number<T> {
+impl<T: Sub<Output = T>> Sub<T> for SubComponent<T> {
     type Output = T;
 
     fn sub(self, rhs: T) -> Self::Output {
@@ -48,29 +82,68 @@ impl<T: Sub<Output = T>> Sub<T> for Number<T> {
     }
 }
 
-impl<T: Copy + Sub<Output = T>> SubAssign<T> for Number<T> {
+impl<T: Copy + Sub<Output = T>> SubAssign<T> for SubComponent<T> {
     fn sub_assign(&mut self, rhs: T) {
         *self = Self(self.0 - rhs);
     }
 }
 
+pub trait Min {
+    fn min() -> Self;
+}
+
+pub trait Max {
+    fn max() -> Self;
+}
+
+macro_rules! impl_min_max {
+        [$($type:ty),*] => {
+                $(
+                impl Min for $type {
+                    fn min() -> $type {
+                            <$type>::MIN
+                        }
+                }
+
+            impl Max for $type {
+                    fn max() -> $type {
+                            <$type>::MAX
+                        }
+                }
+        )*
+            }
+    }
+
+impl_min_max![usize, u8, u16];
+
 #[cfg(test)]
 mod tests {
-    use crate::Number;
+    use super::SubComponent;
 
     #[test]
-    fn number_works() {
-        let mut number = Number(0_u32);
+    fn sub_component_members_work() {
+        let mut sub_component = SubComponent::new(0_u8);
 
-        number.set(10);
-        println!("{}", number.get());
+        sub_component.set(10);
+        (0..10).for_each(|_| sub_component.increment());
+        (0..8).for_each(|_| sub_component.decrement());
 
-        (0..10).for_each(|_| number.increment());
-        println!("{}", number.get());
+        assert_eq!(sub_component.get(), 12);
+    }
 
-        (0..8).for_each(|_| number.decrement());
-        println!("{}", number.get());
+    #[test]
+    fn sub_component_can_underflow() {
+        let mut sub_component = SubComponent::new(u8::MIN);
+        sub_component.decrement();
 
-        assert_eq!(number.get(), 12);
+        assert_eq!(sub_component.get(), u8::MAX)
+    }
+
+    #[test]
+    fn sub_component_can_overflow() {
+        let mut sub_component = SubComponent::new(u8::MAX);
+        sub_component.increment();
+
+        assert_eq!(sub_component.get(), u8::MIN)
     }
 }
